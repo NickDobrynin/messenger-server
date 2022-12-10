76d2cdd643801dd0286ebe5972f6cb4098952f68
Nick Dobrynin <dobryninnick1@gmail.com>
2022-12-11 02:42:29 +0600

    Fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { Chat } from './modules/chats/entities/chat.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: '//mongo:DmZSyqiDc7JxNt4vWzIB@containers-us-west-164.railway.app:7092',
+      url: 'mongodb://mongo:DmZSyqiDc7JxNt4vWzIB@containers-us-west-164.railway.app:7092',
       synchronize: true,
       useUnifiedTopology: true,
       entities: [User, Chat],
