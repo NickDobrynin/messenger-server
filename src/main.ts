6451d91836019bdade2dbcd4e295731a0b3fe244
Nick Dobrynin <dobryninnick1@gmail.com>
2022-12-11 01:10:17 +0600

    Fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    allowedHeaders: '*',
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['content-type'],
     credentials: true,
   });
   await app.listen(4000);
