9b9893b7276c179711fa175d1b27f71fc5d3fd63
Nick Dobrynin <dobryninnick1@gmail.com>
2022-12-11 00:26:04 +0600

    Fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { UsersModule } from './modules/users/users.module';
 import { User } from './modules/users/entities/user.entity';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatsModule } from './modules/chats/chats.module';
 import { Chat } from './modules/chats/entities/chat.entity';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
