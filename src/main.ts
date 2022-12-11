ec68f3618e7625c2a3c1d9cebb79cef4ed9788c6
Nick Dobrynin <dobryninnick1@gmail.com>
2022-12-11 06:06:15 +0600

    Fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://messenger-client-production.up.railway.app',
+    origin: 'https://messenger-client-production-cafb.up.railway.app/',
     allowedHeaders: '*',
     credentials: true,
   });
